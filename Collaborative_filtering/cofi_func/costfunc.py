79fe7af61e0dc311e4eb7c1224a7b1f8940e7472
lee <lsn648693338@gmail.com>
2016-04-15 00:25:53 +0800

    cost function

---
+++ Collaborative_filtering/cofi_func/costfunc.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def costfunc(params, Y, R, num_users, num_movies, num_features, lambda1):
+    J = 0
+    X = params[0, 0:num_movies*num_features].reshape(num_movies, num_features)
+    Theta = params[0, num_movies*num_features:].reshape(num_users, num_features)
+
+    temp = (np.dot(X, Theta.T) - Y) * R
+    J = 1/2 * (temp**2).sum() + lambda1/2 * (Theta**2).sum() + lambda1/2 * (X**2).sum()
+    X_grad = np.dot(temp, Theta) + np.dot(lambda1, X)
+    Theta_grad = np.dot(temp.T, X) + np.dot(lambda1, Theta)
+
+    grad = np.r_[X_grad.reshape(X_grad.size, 1), Theta_grad.reshape(Theta_grad.size, 1)]
+    print('size of X_grad: \n', X_grad.shape)
+    print('size of Theta_grad: \n', Theta_grad.shape)
+    print('X: \n', X)
+    print('Theta: \n', Theta)
+    return J, grad
--- Collaborative_filtering/cofi_func/mainfunc.py
+++ Collaborative_filtering/cofi_func/mainfunc.py
@@ -1,28 +1,39 @@
 import code
 import numpy as np
 from loadMovieList import loadMovieList
+from costfunc import costfunc
 import plotData
 
 print("Plotting Data ...\n")
 Y = np.genfromtxt("../data/movies_Y.csv", delimiter = ',')
 Y = Y[:, 0:-1]
-print(Y)
+print('Y: \n', Y)
 plotData.plotData(Y)
 
 X = np.genfromtxt("../data/movies_X.csv", delimiter = ',')
 X = X[:, 0:-1]
-print(X)
+print('X: \n', X)
 
 R = np.genfromtxt("../data/movies_R.csv", delimiter = ',')
 R = R[:, 0:-1]
-print(R)
+print('R: \n', R)
 
 Theta = np.genfromtxt("../data/movies_Theta.csv", delimiter = ',')
 Theta = Theta[:, 0:-1]
-print(Theta)
+print('Theta: \n', Theta)
 
 movielist = loadMovieList()
-print(movielist)
+#print(movielist)
+
+num_movies = Y.shape[0]
+num_features = 10
+num_users = Y.shape[1]
+
+params = np.c_[X.reshape(1, num_movies*num_features), Theta.reshape(1, num_users*num_features)]
+
+(J, grad) = costfunc(params, Y, R, num_users, num_movies, num_features, 1.5)
+print('J: \n', J)
+print('grad: \n', grad)
 
 pause = code.InteractiveConsole()
 pause.raw_input(prompt = "Press Enter to Continue: ")
